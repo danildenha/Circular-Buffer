5eae263838a0705e94064a23bde06ac058bd9b5d
danildenha <danildenha@gmail.com>
2023-12-13 15:39:34 -0600

    Update Dog.h

---
+++ Dog.h
@@ -0,0 +1,34 @@
+// Dog.h
+#ifndef DOG_H
+#define DOG_H
+#include <iostream>
+using namespace std;
+
+class dog {
+private:
+	string name;
+	string breed;
+	int age;
+
+public:
+	// this is used to initialize the variables of the class
+	dog(string name = "", string breed = "", int age = 0)
+		: name(name), breed(breed), age(age) {}
+
+	string getName() const { return name; }
+
+	string getBreed() const { return breed; }
+
+	int getAge() const { return age; }
+
+	void setBreed(string breed) { this->breed = breed; }
+
+	void setAge(int age) { this->age = age; }
+};
+ostream& operator<<(ostream& out, const dog& d) {
+	out << "\n\t" << d.getName() << ", " << d.getBreed() << ", " << d.getAge();
+	return out;
+}
+#endif
+
+
